998dea0cc18fd52818f54efac9773c2b5d0cc97a
YtalloPereira <ytallopereiralves@gmail.com>
2023-12-12 19:22:35 -0300

    corrigindo a localização de user repository

--- src/main/java/ifpb/edu/br/pj/ifpbichos/model/repository/UserRepository.java
+++ src/main/java/ifpb/edu/br/pj/ifpbichos/model/repository/UserRepository.java
@@ -1,12 +1,12 @@
 package ifpb.edu.br.pj.ifpbichos.model.repository;
-import java.util.Optional;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.security.core.userdetails.UserDetails;
+
 
 import ifpb.edu.br.pj.ifpbichos.model.entity.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
-	public Optional<User> findByLogin(String login);
+	public UserDetails findByLogin(String login);
 }
