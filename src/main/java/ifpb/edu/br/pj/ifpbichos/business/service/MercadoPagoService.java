9044dcc4b6fe664f68fad6635ff159c9bd55c3eb
Igor Sobral <113361588+igorssobral@users.noreply.github.com>
2024-07-19 17:58:40 -0300

    fix: update donations and add exceptions

--- src/main/java/ifpb/edu/br/pj/ifpbichos/business/service/MercadoPagoService.java
+++ src/main/java/ifpb/edu/br/pj/ifpbichos/business/service/MercadoPagoService.java
@@ -1,159 +1,162 @@
 package ifpb.edu.br.pj.ifpbichos.business.service;
 
+import com.mercadopago.client.common.IdentificationRequest;
+import com.mercadopago.client.common.PhoneRequest;
 import com.mercadopago.client.preference.*;
 import com.mercadopago.exceptions.MPApiException;
 import com.mercadopago.exceptions.MPException;
 import com.mercadopago.resources.preference.Preference;
 import ifpb.edu.br.pj.ifpbichos.model.entity.Campaign;
 import ifpb.edu.br.pj.ifpbichos.model.entity.Donation;
-import ifpb.edu.br.pj.ifpbichos.model.entity.Donator;
 import ifpb.edu.br.pj.ifpbichos.model.entity.User;
 import ifpb.edu.br.pj.ifpbichos.model.enums.DonationPaymentStatus;
 import ifpb.edu.br.pj.ifpbichos.model.repository.CampaignRepository;
 import ifpb.edu.br.pj.ifpbichos.model.repository.DonationRepository;
 import ifpb.edu.br.pj.ifpbichos.model.repository.DonatorRepository;
 import ifpb.edu.br.pj.ifpbichos.model.repository.UserRepository;
+import ifpb.edu.br.pj.ifpbichos.presentation.exception.PaymentProcessingException;
+import org.apache.velocity.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 @Service
 public class MercadoPagoService {
 
-    @Autowired
-    private DonatorRepository donatorRepository;
+    private final DonatorRepository donatorRepository;
+    private final DonationRepository donationRepository;
+    private final CampaignRepository campaignRepository;
+    private final UserRepository userRepository;
+    private static final Logger LOGGER = Logger.getLogger(MercadoPagoService.class.getName());
 
     @Autowired
-    private DonationRepository donationRepository;
+    public MercadoPagoService(DonatorRepository donatorRepository, DonationRepository donationRepository,
+                              CampaignRepository campaignRepository, UserRepository userRepository) {
+        this.donatorRepository = donatorRepository;
+        this.donationRepository = donationRepository;
+        this.campaignRepository = campaignRepository;
+        this.userRepository = userRepository;
+    }
 
-    @Autowired
-    private CampaignRepository campaignRepository;
-    @Autowired
-    private UserRepository userRepository;
-
-    public Preference createPayment(String title, String description, BigDecimal transactionAmount, Integer installments,Long campaignId, String userLogin)
-            throws MPException, MPApiException {
-
+    public Preference createPayment(String title, String description, BigDecimal transactionAmount, Integer installments, Long campaignId, String userLogin, String url, Boolean isDirected) throws PaymentProcessingException {
         try {
             PreferenceClient client = new PreferenceClient();
 
-            PreferenceItemRequest itemRequest = PreferenceItemRequest.builder().id("1234").title(title).description(description).categoryId("currency_id").quantity(1).currencyId("BRL").unitPrice(new BigDecimal(String.valueOf(transactionAmount))).build();
+            User user = (User) userRepository.findByLogin(userLogin);
 
-            List<PreferenceItemRequest> items = new ArrayList<>();
-            items.add(itemRequest);
-//
-//        PreferenceFreeMethodRequest freeMethod =
-//                PreferenceFreeMethodRequest.builder()
-//                        .id(1L).build();
-//        List<PreferenceFreeMethodRequest> freeMethodList = new ArrayList<>();
-//        freeMethodList.add(freeMethod);
-//
-//        List<PreferencePaymentTypeRequest> excludedPaymentTypes = new ArrayList<>();
-//        excludedPaymentTypes.add(PreferencePaymentTypeRequest.builder().id("ticket").build());
-//
-//        List<PreferencePaymentMethodRequest> excludedPaymentMethods = new ArrayList<>();
-//        excludedPaymentMethods.add(PreferencePaymentMethodRequest.builder().id("").build());
-            String successUrl = "http://localhost:5173/success";
-            PreferenceRequest preferenceRequest = PreferenceRequest.builder()
-                    .backUrls(PreferenceBackUrlsRequest.builder()
-                            .success("http://localhost:5173/campanhas")
-                            .failure("http://localhost:5173/campanhas")
-                            .pending("http://localhost:5173/pending")
-                            .build()).differentialPricing(PreferenceDifferentialPricingRequest.builder()
-                            .id(1L).build()).expires(false).items(items)
-                    .marketplaceFee(new BigDecimal("0"))
-                    .autoReturn("all")
-                    .build();
-//              .payer(
-//                        PreferencePayerRequest.builder()
-//                                .name("Test")
-//                                .surname("User")
-//                                .email("your_test_email@example.com")
-//                                .phone(PhoneRequest.builder().areaCode("11").number("4444-4444").build())
-//                                .identification(
-//                                        IdentificationRequest.builder().type("CPF").number("19119119100").build())
-//                                .build())
-//                .autoReturn("all")
-//                .binaryMode(true)
-//                .externalReference("1643827245")
-//                .marketplace("marketplace")
-//                .notificationUrl("http://notificationurl.com")
-//                .operationType("regular_payment")
-//                .paymentMethods(
-//                        PreferencePaymentMethodsRequest.builder()
-//                                .defaultPaymentMethodId("master")
-//                                .excludedPaymentTypes(excludedPaymentTypes)
-//                                .excludedPaymentMethods(excludedPaymentMethods)
-//                                .installments(5)
-//                                .defaultInstallments(1)
-//                                .build())
-//                .shipments(
-//                        PreferenceShipmentsRequest.builder()
-//                                .mode("custom")
-//                                .localPickup(false)
-//                                .defaultShippingMethod(null)
-//                                .freeMethods(freeMethodList)
-//                                .cost(BigDecimal.TEN)
-//                                .freeShipping(false)
-//                                .dimensions("10x10x20,500")
-//                                .build())
-//                .statementDescriptor("Test Store")
-//                .build();
+            PreferenceRequest preferenceRequest = buildPreferenceRequest(title, description, transactionAmount, installments, user, url);
 
             Preference preference = client.create(preferenceRequest);
-            System.out.println(preference.getInitPoint());
 
-            SaveDonation(preference.getId(), transactionAmount, campaignId, userLogin);
+            saveDonation(preference.getId(), transactionAmount, campaignId, userLogin, isDirected);
 
             return preference;
-        } catch (MPException e) {
-            throw new RuntimeException(e);
-        } catch (MPApiException e) {
-            throw new RuntimeException(e);
+        } catch (MPException | MPApiException e) {
+            throw new PaymentProcessingException("Failed to create payment", e);
         }
     }
 
-    public Donation updatePayment(String preferenceId,String paymentId,String status,String paymentType) throws Exception {
-        Optional<Donation> donation = donationRepository.findByPreferenceId(preferenceId);
-        Optional<Campaign> campaign = campaignRepository.findById(donation.get().getCampaign().getId());
-        if(!donation.isPresent()){
-           throw new Exception("Erro ao atualizar");
+    private PreferenceRequest buildPreferenceRequest(String title, String description, BigDecimal transactionAmount, Integer installments, User user, String url) {
+        PreferenceItemRequest itemRequest = PreferenceItemRequest.builder()
+                .id(UUID.randomUUID().toString())
+                .title(title)
+                .description(description)
+                .categoryId("currency_id")
+                .quantity(1)
+                .currencyId("BRL")
+                .unitPrice(transactionAmount)
+                .build();
 
-        }
-        campaign.get().setBalance(campaign.get().getBalance().add(donation.get().getDonationValue()));
+        List<PreferenceItemRequest> items = Collections.singletonList(itemRequest);
 
-        campaignRepository.save(campaign.get());
-
-        Donation donationUpdate = donation.get();
-        donationUpdate.setPaymentId(paymentId);
-        donationUpdate.setStatus(DonationPaymentStatus.APPROVED);
-        donationUpdate.setPaymentType(paymentType);
-        System.out.println(donationUpdate);
-        return donationRepository.save(donationUpdate);
+        return PreferenceRequest.builder()
+                .backUrls(PreferenceBackUrlsRequest.builder()
+                        .success(url)
+                        .failure("http://localhost:5173/campanhas")
+                        .pending("http://localhost:5173/pending")
+                        .build())
+                .differentialPricing(PreferenceDifferentialPricingRequest.builder()
+                        .id(1L)
+                        .build())
+                .expires(false)
+                .items(items)
+                .marketplaceFee(BigDecimal.ZERO)
+                .autoReturn("all")
+                .payer(buildPayerRequest(user))
+                .binaryMode(true)
+                .externalReference("1643827245")
+                .marketplace("marketplace")
+                .notificationUrl("http://notificationurl.com")
+                .operationType("regular_payment")
+                .build();
     }
 
-    public void SaveDonation(String preferenceId, BigDecimal transactionAmount,Long campaignId, String userLogin) {
+    private PreferencePayerRequest buildPayerRequest(User user) {
+        return PreferencePayerRequest.builder()
+                .name(user.getName())
+                .email(user.getEmail())
+                .phone(PhoneRequest.builder()
+                        .areaCode(user.getPhoneNumber().substring(0, 2))
+                        .number(user.getPhoneNumber().substring(2))
+                        .build())
+                .identification(IdentificationRequest.builder()
+                        .type("CPF")
+                        .number(user.getCPF())
+                        .build())
+                .build();
+    }
+    public Donation updatePayment(String preferenceId, String paymentId, String status, String paymentType) {
+
+        Donation donation = donationRepository.findByPreferenceId(preferenceId)
+                .orElseThrow(() -> new ResourceNotFoundException("Donation not found"));
+
+        Campaign campaign = campaignRepository.findById(donation.getCampaign().getId())
+                .orElseThrow(() -> new ResourceNotFoundException("Campaign not found"));
+
+        if(donation.getPaymentId() == null){
+            campaign.setBalance(campaign.getBalance().add(donation.getDonationValue()));
+            campaignRepository.save(campaign);
+
+            donation.setPaymentId(paymentId);
+            donation.setStatus(DonationPaymentStatus.APPROVED);
+            donation.setPaymentType(paymentType);
+        }
+
+
+        return donationRepository.save(donation);
+    }
+
+    public void saveDonation(String preferenceId, BigDecimal transactionAmount,Long campaignId, String userLogin, Boolean isDirected) {
 
         User user = (User) userRepository.findByLogin(userLogin);
-        System.out.println(user.getLogin());
-        System.out.println(campaignId);
-        Optional<Campaign> campaign = campaignRepository.findById(campaignId);
+
+        if (user == null) {
+            throw new ResourceNotFoundException("User not found");
+        }
+
+        Campaign campaign = null;
+        if (isDirected) {
+            campaign = campaignRepository.findById(campaignId)
+                    .orElseThrow(() -> new ResourceNotFoundException("Campaign not found"));
+        }
+
         Donation donation = new Donation();
 
         donation.setDate(LocalDateTime.now());
-        donation.setCampaign(campaign.get());
-        donation.setDirected(false);
+        donation.setCampaign(campaign);
+        donation.setDirected(isDirected);
         donation.setPreferenceId(preferenceId);
         donation.setDonationValue(transactionAmount);
         donation.setStatus(DonationPaymentStatus.PENDING);
-        user.getDonations().add(donation);
         donation.setDonator(user);
 
+        user.getDonations().add(donation);
 
         donationRepository.save(donation);
     }
