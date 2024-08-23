9cdd21a1cbeced4d3a7566731ae296aada0da538
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-08-23 18:06:03 -0300

    :recycle: refactor: refatorando a classe de ResourcesRealcation

--- src/main/java/ifpb/edu/br/pj/ifpbichos/model/entity/ResourcesRealocation.java
+++ src/main/java/ifpb/edu/br/pj/ifpbichos/model/entity/ResourcesRealocation.java
@@ -5,23 +5,30 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 @Entity
-public class ResourcesRealocation {
+public class ResourcesRealocation implements Serializable {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne
+    @JoinColumn(name = "CAMPAIGN_ID", nullable = false)
     private Campaign campaign;
 
-    private Date date;
+    private LocalDateTime date;
 
     private BigDecimal value;
 }
