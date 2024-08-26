e6a0535058ead8cb6b1aef7c5d03c710e61c0cca
YtalloAlves <ytollo.alves@academico.ifpb.edu.br>
2024-08-26 18:33:22 -0300

    :sparkles: feat: adicionando a classe CampaignActionResource que representará as aplicações de recursos

---
+++ src/main/java/ifpb/edu/br/pj/ifpbichos/model/entity/CampaignActionResource.java
@@ -0,0 +1,47 @@
+package ifpb.edu.br.pj.ifpbichos.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity(name = "CAMPAIGN_ACTION_RESOURCE")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CampaignActionResource implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "CAMPAIGN_ID", nullable = false)
+    private Campaign campaign;
+
+    @Column(nullable = false, name = "JUSTIFICATION")
+    private String justification;
+
+    @Column(nullable = false, name = "ACTION")
+    private String action;
+
+    @Column(nullable = false, name = "COST")
+    private BigDecimal cost;
+
+    @Column(nullable = false, name = "DATE")
+    private LocalDateTime completionDate;
+
+    @Lob
+    @Column(name = "RECEIPT_IMAGE")
+    private byte[] receipt;
+
+    @Column(name = "UNDIRECTED_BALANCE_USED")
+    private BigDecimal withdrawalFromUndirectedBalance;
+
+
+}
