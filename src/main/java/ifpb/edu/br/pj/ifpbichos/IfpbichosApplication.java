04c7b6f7b078b6e0160773e5a9ad2690e4f62822
igorssobral <igorgmz1421@gmail.com>
2024-12-10 18:06:47 -0300

    .

--- src/main/java/ifpb/edu/br/pj/ifpbichos/IfpbichosApplication.java
+++ src/main/java/ifpb/edu/br/pj/ifpbichos/IfpbichosApplication.java
@@ -14,7 +14,7 @@ public class IfpbichosApplication implements WebMvcConfigurer, CommandLineRunner
 
 	public static void main(String[] args){
 
-		Dotenv dotenv = Dotenv.configure().load();
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
